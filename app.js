ef808299d3138acce7d3a1659bb5122efa5cceef
Bendark84 <benrc84@hotmail.com>
2022-11-30 22:12:15 -0600

    Mejorando la documentacion

--- app.js
+++ app.js
@@ -29,11 +29,11 @@ app.use('/api/v1/cart', cartsRoutes);
 // Global error handler
 app.use(globalErrorHandler);
 
-app.all('*', (req, res) => {
-  res.status(404).json({
-    status: 'error',
-    message: `${req.method} ${req.url} does not exists in our server`,
-  });
-});
+// app.all('*', (req, res) => {
+//   res.status(404).json({
+//     status: 'error',
+//     message: `${req.method} ${req.url} does not exists in our server`,
+//   });
+// });
 
 module.exports = { app };
